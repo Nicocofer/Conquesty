2d0ac7dfc3bf37f7837125333700824db265d96c
ferreira <guillaumeferreira00@gmail.com>
2017-12-24 10:58:00 +0100

    rectification balise métal

--- index.py
+++ index.py
@@ -11,8 +11,8 @@ metal="15"
 file = open("index.html", "r")
 html=""
 for line in file:
-    if line.strip()=="<p id=\"metal\">mÃ©tal</p>":
-        html= html +"<p id=\"metal\">mÃ©tal " + metal + "</p>"
+    if line.strip()=="<p id=\"metal\"></p>":
+        html= html +"<p id=\"metal\">" + metal + "</p>"
     else:
         html= html + line
     
