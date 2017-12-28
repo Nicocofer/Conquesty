d3f9dcadba2641bd5085e532370c60cdd7a67f5f
ferreira <guillaumeferreira00@gmail.com>
2017-12-28 10:41:39 +0100

    rajout limite population et calcul pour ne pas depasser la limite

--- utils/moteur_planete.py
+++ utils/moteur_planete.py
@@ -6,22 +6,25 @@ import random
 def population():
     conn = sqlite3.connect('Base_conquesty.db3')
     cursor = conn.cursor()
-    cursor.execute("SELECT id, population,type FROM planete")
+    cursor.execute("SELECT id, population,type,population_max FROM planete")
     
  
     for row in cursor.fetchall():
         
         pop = row[1]
-        if pop < 1000:
-            if row[2]==3:
-                pop=pop + float(pop)*0.01*random.randrange(9)
-            else:
-                pop=pop + float(pop)*0.01*random.randrange(4)
+        if pop >= row[3]:
+            pop = row[3]
         else:
-            if row[2]==3:
-                pop=pop + float(pop)*0.001*random.randrange(9)
+            if pop < 1000:
+                if row[2]==3:
+                    pop=pop + float(pop)*0.01*random.randrange(9)
+                else:
+                    pop=pop + float(pop)*0.01*random.randrange(4)
             else:
-                pop=pop + float(pop)*0.001*random.randrange(4)
+                if row[2]==3:
+                    pop=pop + float(pop)*0.001*random.randrange(9)
+                else:
+                    pop=pop + float(pop)*0.001*random.randrange(4)
         print(pop)
         
         cursor.execute("UPDATE Planete SET population = "+str(int(pop))+" WHERE ID =" +str(row[0]))
