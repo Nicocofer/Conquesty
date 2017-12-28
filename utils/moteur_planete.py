9c7966b1b199e45cc91c54c90bcc2b6d99c23609
ferreira <guillaumeferreira00@gmail.com>
2017-12-28 10:17:54 +0100

    calcul de la pop en fonction du type de planete et passage du tour a 30 sc

--- utils/moteur_planete.py
+++ utils/moteur_planete.py
@@ -6,14 +6,23 @@ import random
 def population():
     conn = sqlite3.connect('Base_conquesty.db3')
     cursor = conn.cursor()
-    cursor.execute("SELECT id, population FROM planete")
+    cursor.execute("SELECT id, population,type FROM planete")
     
  
     for row in cursor.fetchall():
         
         pop = row[1]
-        pop=pop + float(pop)*0.01
-        
+        if pop < 1000:
+            if row[2]==3:
+                pop=pop + float(pop)*0.01*random.randrange(9)
+            else:
+                pop=pop + float(pop)*0.01*random.randrange(4)
+        else:
+            if row[2]==3:
+                pop=pop + float(pop)*0.001*random.randrange(9)
+            else:
+                pop=pop + float(pop)*0.001*random.randrange(4)
+        print(pop)
         
         cursor.execute("UPDATE Planete SET population = "+str(int(pop))+" WHERE ID =" +str(row[0]))
         conn.commit()
