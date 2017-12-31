46e7ce58a066e0e58b806a6aad7ac4ce4a90b8a8
nico <nicocofer@hotmail.com>
2017-12-31 12:02:08 +0100

    changement des types de planètes de id->class

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,32 @@
-function openbox(){$(".Pdescription").css("display","flex"),$(".planet").css("display","none"),console.log(this)}function closebox(){$(".Pdescription").css("display","none"),$(".planet").css("display","flex")}closebox();
+// $( ".planet" ).click(function() {
+
+// 	console.log('Planète de '+this.id);
+
+// 	$("#planetebox").html('<div class="Pdescription"></div>');
+// 	$(".Pdescription").html('<div id="close"></div>');
+
+// 	alert('truc');
+
+// });
+
+// $('#close').click(function(){
+// 		$(".Pdescription").css('display','none');
+// 		$("#planetebox").html('<div class="planet" id="type0"></div><div class="planet" id="type1"></div><div class="planet" id="type2"></div><div class="planet" id="type3"></div>');
+		
+
+
+// 	});
+
+
+function openbox() {
+   $(".Pdescription").css("display","flex");
+   $(".planet").css("display","none");
+   console.log(this);
+}
+
+function closebox() {
+     $(".Pdescription").css('display','none');
+     $(".planet").css("display","flex");
+}
+
+closebox();
