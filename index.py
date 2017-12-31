9257712676cf58dc1d673ef05c9e31ea42837fc7
ferreira <guillaumeferreira00@gmail.com>
2017-12-31 12:07:49 +0100

    rajout id unique html

--- index.py
+++ index.py
@@ -36,12 +36,14 @@ for line in file:
         html= html +"<p id=\"gaz\">" + gaz+"/"+ gaz_max  + "</p>"
     elif line.strip()=="<p id=\"energie\"></p>":
         html= html +"<p id=\"energie\">" + energie + "</p>"
-    elif line.strip()== "<div class=\"planet\" id=\"type0\" style=\"top:30%;left:30%;\" onclick=\"openbox()\"></div>":
-        cursor.execute("""SELECT x,y,type FROM Planete WHERE systeme=0 and galaxie=1""")
+    elif line.strip()== "<div class=\"planet type0\" id=\"\" style=\"top:30%;left:30%;\" onclick=\"openbox()\"></div>":
+        
+        cursor.execute("""SELECT x,y,type,id FROM Planete WHERE systeme=0 and galaxie=1""")
         for row in cursor:
             x = row[0]
             y = row[1]
             type_planete = row[2]
+            id_planete = row[3]
             if x >= 0:
                 x=50+ (x/100)
             else:
@@ -50,7 +52,7 @@ for line in file:
                 y=50+ (y/100)
             else:
                 y=(y+500)/100
-            html= html +"<div class=\"planet\" id=\"type"+ str(type_planete) +"\" style=\"top:"+str(y)+"%;left:"+str(x)+"%;\" onclick=\"openbox()\"></div>"
+            html= html +"<div class=\"planet type"+str(type_planete)+"\" id=\""+ str(id_planete) +"\" style=\"top:"+str(y)+"%;left:"+str(x)+"%;\" onclick=\"openbox()\"></div>"
                 
     else:
         html= html + line
