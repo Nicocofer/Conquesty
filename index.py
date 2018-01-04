653b642a71c3c26e7e61dba67ae7f1f9208f162b
ferreira <guillaumeferreira00@gmail.com>
2018-01-04 07:08:04 +0100

    changement de serveur, passage au cherrypy

--- index.py
+++ index.py
@@ -6,65 +6,66 @@ import re
 import sqlite3
 from class_object.Planete import Planete
 
-form = cgi.FieldStorage()
+#form = cgi.FieldStorage()
+def index_conquesty():
+    conn = sqlite3.connect('Base_conquesty.db3')
+    cursor = conn.cursor()
 
-conn = sqlite3.connect('Base_conquesty.db3')
-cursor = conn.cursor()
+    print("Content-type: text/html; charset=utf-8\n")
 
-print("Content-type: text/html; charset=utf-8\n")
+    #recuperation
+    cursor.execute("""SELECT id FROM Planete WHERE systeme=1 and galaxie=1""")
 
-#recuperation
-cursor.execute("""SELECT id FROM Planete WHERE systeme=1 and galaxie=1""")
+    liste_planete_systeme=[]
+    for row in cursor:
 
-liste_planete_systeme=[]
-for row in cursor:
+        planete = Planete()
 
-    planete = Planete()
-
-    planete.affichage_planete(row[0])
-    liste_planete_systeme.append(planete)
+        planete.affichage_planete(row[0])
+        liste_planete_systeme.append(planete)
 
 
 
-    
-file = open("index.html", "r")
-html=""
-for line in file:
-    if line.strip()=="<p id=\"metal\"></p>":
-        html= html +"<p id=\"metal\">" + liste_planete_systeme[1].metal +"/"+ liste_planete_systeme[1].metal_max + "</p>"
-    elif line.strip()=="<p id=\"cristal\"></p>":
-        html= html +"<p id=\"cristal\">" + liste_planete_systeme[1].cristal+"/"+ liste_planete_systeme[1].cristal_max  + "</p>"
-    elif line.strip()=="<p id=\"gaz\"></p>":
-        html= html +"<p id=\"gaz\">" + liste_planete_systeme[1].gaz+"/"+ liste_planete_systeme[1].gaz_max  + "</p>"
-    elif line.strip()=="<p id=\"energie\"></p>":
-        html= html +"<p id=\"energie\">" + liste_planete_systeme[1].energie + "</p>"
-    elif line.strip()== "<div class=\"boxP\" id=\"\"><div class=\"headP\"><div class=\"nameP\">Name planet</div><div class=\"close\" onclick=\"closebox()\"><span>X</span></div></div></div>":
-           for planete in liste_planete_systeme:
-               id_planete = planete.id
-               nom_planete = planete.nom
-               html= html + "<div class=\"boxP\" id=\"p"+id_planete+"\"><div class=\"headP\"><div class=\"nameP\">"+nom_planete+"</div><div class=\"close\" onclick=\"closebox()\"><span>X</span></div></div></div>"
-               
-    elif line.strip()== "<div class=\"planet type0\" id=\"\" style=\"top:30%;left:30%;\" onclick=\"openbox()\"></div>":
         
-        for planete in liste_planete_systeme:
-            x = planete.x
-            y = planete.y
-            x = float(x)
-            y = float(y)
-            type_planete = planete.type
-            id_planete = planete.id
-            if x >= 0:
-                x=50+ (x/100)
-            else:
-                x=(x+500)/100
-            if y >= 0:
-                y=50+ (y/100)
-            else:
-                y=(y+500)/100
-            html= html +"<div class=\"planet type"+str(type_planete)+"\" id=\""+ str(id_planete) +"\" style=\"top:"+str(y)+"%;left:"+str(x)+"%;\" onclick=\"openbox()\"></div>"
-                
-    else:
-        html= html + line
-    
+    file = open("index.html", "r")
+    html=""
+    for line in file:
+        if line.strip()=="<p id=\"metal\"></p>":
+            html= html +"<p id=\"metal\">" + liste_planete_systeme[1].metal +"/"+ liste_planete_systeme[1].metal_max + "</p>"
+        elif line.strip()=="<p id=\"cristal\"></p>":
+            html= html +"<p id=\"cristal\">" + liste_planete_systeme[1].cristal+"/"+ liste_planete_systeme[1].cristal_max  + "</p>"
+        elif line.strip()=="<p id=\"gaz\"></p>":
+            html= html +"<p id=\"gaz\">" + liste_planete_systeme[1].gaz+"/"+ liste_planete_systeme[1].gaz_max  + "</p>"
+        elif line.strip()=="<p id=\"energie\"></p>":
+            html= html +"<p id=\"energie\">" + liste_planete_systeme[1].energie + "</p>"
+        elif line.strip()== "<div class=\"boxP\" id=\"\"><div class=\"headP\"><div class=\"nameP\">Name planet</div><div class=\"close\" onclick=\"closebox()\"><span>X</span></div></div></div>":
+               for planete in liste_planete_systeme:
+                   id_planete = planete.id
+                   nom_planete = planete.nom
+                   html= html + "<div class=\"boxP\" id=\"p"+id_planete+"\"><div class=\"headP\"><div class=\"nameP\">"+nom_planete+"</div><div class=\"close\" onclick=\"closebox()\"><span>X</span></div></div></div>"
+                   
+        elif line.strip()== "<div class=\"planet type0\" id=\"\" style=\"top:30%;left:30%;\" onclick=\"openbox()\"></div>":
+            
+            for planete in liste_planete_systeme:
+                x = planete.x
+                y = planete.y
+                x = float(x)
+                y = float(y)
+                type_planete = planete.type
+                id_planete = planete.id
+                if x >= 0:
+                    x=50+ (x/100)
+                else:
+                    x=(x+500)/100
+                if y >= 0:
+                    y=50+ (y/100)
+                else:
+                    y=(y+500)/100
+                html= html +"<div class=\"planet type"+str(type_planete)+"\" id=\""+ str(id_planete) +"\" style=\"top:"+str(y)+"%;left:"+str(x)+"%;\" onclick=\"openbox()\"></div>"
+                    
+        else:
+            html= html + line
+        
 
-print(html)
+    return html
+
--- serveur.py
+++ serveur.py
@@ -2,9 +2,49 @@
 # -*- coding: utf-8 -*
 import http.server
 import threading
+import os
 import time
 from utils.moteur_planete import population
+from index import index_conquesty
+import dist
+import cherrypy
 
+class Authentification(object):
+    @cherrypy.tools.sessions()
+    def index(self):
+        return """<html>
+          <head></head>
+          <body>
+            <form method="post" action="conquesty">
+              Login : <input type="text" value="" name="msg" />
+              <button type="submit">Valider</button>
+            </form>
+          </body>
+        </html>"""
+    index.exposed = True
+
+    
+    @cherrypy.tools.sessions()
+    def conquesty(self, msg):
+        html = index_conquesty()
+        cherrypy.session['user'] = msg
+        return html.encode('latin1')
+    conquesty.exposed = True
+    @cherrypy.tools.sessions()
+    def page2(self):
+        return cherrypy.session['user'] + "<a href='page1'> Suivant</a>"
+    page2.exposed = True
+    @cherrypy.tools.sessions()
+    def page1(self):
+        return cherrypy.session['user'] + "<a href='page2'> Suivant</a>"
+    page1.exposed = True
+    
+def serveur():
+    configfile=os.path.join(os.path.dirname(__file__),r"config.conf")
+    application = cherrypy.tree.mount(Authentification(), '/')
+    cherrypy.quickstart(application, config=configfile)
+    
+"""    
 def serveur():
     PORT = 8888
     server_address = ("", PORT)
@@ -15,7 +55,7 @@ def serveur():
     print("Serveur actif sur le port :", PORT)
 
     httpd = server(server_address, handler)
-    httpd.serve_forever()
+    httpd.serve_forever()"""
 
 
 def moteur_conquesty():
