2faf47e6dee3dcd83d28ce5b89d716f12887127e
nico <nicocofer@hotmail.com>
2018-01-06 21:01:58 +0100

    première version de la Pdescription avec stats

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,12 @@
+
+
 $( ".planet" ).click(function() {
 
 	n = '#p'+this.id ;
 
 	$(".boxP").css('display','none');
 	$(n).css('display','flex');
-	console.log(n);
-
 });
-
-// $('#close').click(function(){
-// 		$(".Pdescription").css('display','none');
-// 		$("#planetebox").html('<div class="planet" id="type0"></div><div class="planet" id="type1"></div><div class="planet" id="type2"></div><div class="planet" id="type3"></div>');
-		
-
-
-// 	});
-
 
 function openbox() {
    $(".Pdescription").css("display","flex");
@@ -28,4 +19,4 @@ function closebox() {
      // $(".planet").css("display","flex");
 }
 
-closebox();
+// closebox();
