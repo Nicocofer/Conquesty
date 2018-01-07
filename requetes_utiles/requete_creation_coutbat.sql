83d8c99d91290a1c21309f41cf9d774cdfae3378
ferreira <guillaumeferreira00@gmail.com>
2018-01-07 15:33:10 +0100

    ajout liste cout dans BDD

---
+++ requetes_utiles/requete_creation_coutbat.sql
@@ -0,0 +1,10 @@
+insert into CoutBatiment values (NULL,4,1,500,800,0,20);
+insert into CoutBatiment values (NULL,4,2,800,1500,0,10);
+insert into CoutBatiment values (NULL,4,3,1500,3000,0,10);
+insert into CoutBatiment values (NULL,4,4,2700,5000,0,10);
+insert into CoutBatiment values (NULL,4,5,5500,9000,0,10);
+insert into CoutBatiment values (NULL,4,6,10000,17000,0,10);
+insert into CoutBatiment values (NULL,4,7,20000,30000,0,10);
+insert into CoutBatiment values (NULL,4,8,35000,50000,0,10);
+insert into CoutBatiment values (NULL,4,9,70000,90000,0,10);
+insert into CoutBatiment values (NULL,4,10,100000,150000,0,10);
---
+++ requetes_utiles/requete_creation_table_coutbat.sql
@@ -0,0 +1,10 @@
+CREATE TABLE CoutBatiment(
+     id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+     type INTEGER,
+     niveau INTEGER,
+     metal INTEGER,
+     cristal INTEGER,
+     gaz INTEGER,
+     energie INTEGER
+     
+)
